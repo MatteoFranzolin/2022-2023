b960019ffae46d0b4645e18ff77f8ed89e90e083
Matteo Franzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2022-11-13 17:28:37 +0100

    Es2 gestione file

---
+++ Es2_Laboratorio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 81
+/* ricerca di parole in un file di testo */
+int main()
+{
+    FILE *f1;
+    char P[MAX], S[MAX];
+    int contatore = 0;
+    f1 = fopen("F1.txt", "r");
+    if (f1 == NULL)
+    {
+        printf("Errore apertura del file!\n");
+        return 0;
+    }
+    fscanf(f1, "%s", S);
+    while (!feof(f1))
+    {
+        contatore++;
+        fscanf(f1, "%s", S);
+    }
+    printf("Il file contiene %d parole\n", contatore);
+    fclose(f1);
+}
